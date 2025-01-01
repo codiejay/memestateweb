406d99099f0a02b247ace4c3f46204a8690be8c0
UzochukwuEmeka <uzochukwuemeka516@gmail.com>
2025-01-01 08:47:52 +0100

    update

--- src/component/VideoModal/VideoModal.jsx
+++ src/component/VideoModal/VideoModal.jsx
@@ -4,7 +4,7 @@ const VideoModal = () => {
   const { closeModal } = useModal();
   return (
     <div className="fixed  w-screen z-[9999999] justify-center flex  lg:h-screen h-fit top-10">
-      <div className="border-[12px] border-[#282B7F] h-[647px] max-w-[1102px] rounded-[32px] overflow-hidden w-full bg-black">
+      <div className="border-[12px] border-[#282B7F] lg:h-[647px] h-full max-w-[1102px] rounded-[32px] overflow-hidden w-full bg-black">
         <div className="w-full border-[10px] border-white h-full relative">
           <div className="absolute right-4 z-50">
             <button onClick={closeModal}>
