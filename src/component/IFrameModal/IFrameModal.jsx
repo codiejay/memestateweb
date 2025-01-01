d46e387ded758ae08dcf47afd959251e0389a2ec
UzochukwuEmeka <uzochukwuemeka516@gmail.com>
2025-01-01 17:06:25 +0100

    Added a background image to the body. Centered the modal on larger screens.

--- src/component/IFrameModal/IFrameModal.jsx
+++ src/component/IFrameModal/IFrameModal.jsx
@@ -3,7 +3,7 @@ import { useIframeModal } from "../../context/IframeContext";
 const IFrameModal = () => {
   const { closeModal } = useIframeModal();
   return (
-    <div className="fixed  w-screen z-[9999999] justify-center flex  h-screen items-center  top-10">
+    <div className="fixed  w-screen z-[9999999] justify-center flex  h-screen items-center  top-10 max-w-[1600px] mx-auto">
       <div className="border-[16px] border-[#000] h-[647px] max-w-[1102px] rounded-[32px] w-full bg-black">
         <div className="w-full border-[10px] border-white h-full  relative overflow-hidden ">
           <div className="absolute right-4">
