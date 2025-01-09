6f55e186926375ce45ad6491720762bf65a9a4ed
UzochukwuEmeka <uzochukwuemeka516@gmail.com>
2025-01-09 13:21:08 +0100

    Fix height of Glass Card

--- src/component/GlassCard/GlassCard.jsx
+++ src/component/GlassCard/GlassCard.jsx
@@ -2,12 +2,16 @@ const GlassCard = ({ maxWidth, image, content }) => {
   return (
     <div className="flex flex-col items-center justify-center h-full    ">
       <div className="-mb-10 z-50">
-        <img src={`/images/${image}`} alt="road1" className="h-[107px]" />
+        <img
+          src={`/images/${image}`}
+          alt="road1"
+          className="lg:h-[107px]  lg:min-h-['unset'] min-h-full h-full "
+        />
       </div>
 
       <div>
         <div
-          className={`roadMapCard h-[540px]  lg:px-0 px-5  lg:py-0 py-8 glassCardContent`}
+          className={`roadMapCard lg:h-[540px]  lg:min-h-['unset] min-h-[540px] lg:px-0 px-5  lg:py-0 py-8 glassCardContent`}
           style={{ maxWidth: maxWidth }}
         >
           {content}
