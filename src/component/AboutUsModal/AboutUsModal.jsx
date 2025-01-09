b5420fc6bd4120b8009271cf576a5f38ed56aed0
UzochukwuEmeka <uzochukwuemeka516@gmail.com>
2025-01-09 16:03:14 +0100

    Adjust Height to of Modal text

--- src/component/AboutUsModal/AboutUsModal.jsx
+++ src/component/AboutUsModal/AboutUsModal.jsx
@@ -37,7 +37,7 @@ const AboutUsModal = ({ img, alt, name, title, content, borderColor }) => {
               </p>
             </div>
           </div>
-          <p className="lg:text-[26px] text-3lg font-normal text-black font-poppins overflow-auto lg:h-[unset] h-[80vh]   mt-4">
+          <p className="lg:text-[26px] text-3lg font-normal text-black font-poppins overflow-auto lg:h-[unset] h-[70vh]   mt-4">
             {content}
           </p>
         </div>
